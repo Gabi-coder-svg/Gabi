c675684d0923db13d6e1fe4ef9650735662a707d
Netto011 <arlindogoncalvesdearaujoneto@gmail.com>
2025-02-07 21:12:28 -0300

    uptade

--- success.js
+++ success.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const produtoId = params.get('produto');
 
     const produtos = [
-        { id: 1, nome: "Curso de Marketing Digital", driveLink: "https://drive.google.com/file/d/SEU_DRIVE_ID_1/view?usp=sharing" },
+        { id: 1, nome: "pack 1", driveLink: "https://drive.google.com/drive/folders/1WZJyazo8IHjEitpVG_y-QVlm3Naal_tM?usp=sharing" },
         { id: 2, nome: "Template de Site Profissional", driveLink: "https://drive.google.com/file/d/SEU_DRIVE_ID_2/view?usp=sharing" },
         { id: 3, nome: "Pack de Ícones Premium", driveLink: "https://drive.google.com/file/d/SEU_DRIVE_ID_3/view?usp=sharing" },
     ];
